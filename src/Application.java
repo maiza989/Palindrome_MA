7cdeba935e1df74720ad17f21251b99a91c8520b
maitham h <malghamgham@bellermine.edu>
2019-10-02 00:05:50 -0400

    Second commit

--- src/Application.java
+++ src/Application.java
@@ -7,8 +7,25 @@ public class Application {
 	public static void main(String[] args) {
 	
 	Palindrome p = new Palindrome();
-	p.test();
-
+	p.input();
+/*
+	static boolean isSubSequence(String str1, String str2, int m, int n) {
 		
+		int j = 0; 
+	
+		for (int i = 0; i < str2.length(); i++) 
+				if (str1.charAt(j) == str2.charAt(i)) {
+						j++; 
+				}// end of if 1
+		if(j == str1.length()) {
+			
+			return true;
+			
+		}// end of if 2
+		
+			return false;
+			
+		}// end of isSubSequence
+		*/
 	}// end if main 
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,41 +1,41 @@
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Palindrome {
 
-	private Scanner x;
 	
 	/**
 	 * A Method that compare the First String characters with the second string and see if it match
-	 * and return  if all the characters are found
+	 * and return  true else return false.
 	 * @param str1
 	 * @param str2
 	 * @param m
 	 * @param n
 	 * @return
 	 */
-	static boolean isSubSequence(String str1, String str2, int m, int n) {
+	public boolean isSubSequence(String str1, String str2, int m, int n) {
 			
 		int j = 0; 
 	
 		for (int i = 0; i < n && j < m; i++) 
-				if (str1.charAt(j) == str2.charAt(i)) 
-						j++; 
+            if (str1.charAt(j) == str2.charAt(i)) 
+                j++; 
+				
+		return(j == m);
 		
-			return (j == m);  
 		}// end of isSubSequence
 	
 	public void test() {
 		
 		
-		String s = "abc";
-		String s2 = "abc";
-		int l1 = s.length();
-		int l2 = s2.length();
-		
-		boolean r = isSubSequence(s, s2, l1, l2);
+		String s = "adc";
+		String s2 = "aqdec";
+		int m = s.length();
+		int n = s2.length();
+		boolean r = isSubSequence(s, s2, m , n);
 		if(r) {
 			
 			System.out.println(s + " IS A SUBSEQUENCE of " + s2);
@@ -47,92 +47,138 @@ public class Palindrome {
 			
 		}
 	}
-	
-	public void Strings() {
+	/**
+	 * A method that gets an input of two different string from the user
+	 * and check if the first string is a subsequence of the second string.
+	 */
+	public void manual() {
 		
 		Scanner scan = new Scanner (System.in);
-				
-		System.out.println("Enter the first sequence:" );
-		scan.nextLine();
+		String userInput = "";
+		String userInputTwo = "";
+		int m = userInput.length();
+		int n = userInputTwo.length();
+		
+			
+		System.out.println("Enter the first sequence:");
+		
+		userInput = scan.nextLine();
 		
 		System.out.println("Enter the second sequence:");
-		scan.nextLine();
-		scan.close();
+		
+		userInputTwo = scan.nextLine();
+		
+		boolean r = isSubSequence(userInput, userInputTwo, m , n);
+		if(r) {
+			
+			System.out.println(userInput + " IS A SUBSEQUENCE of " + userInputTwo);
+			
+		}
+		else {
+			
+			System.out.println( userInput +" IS NOT A SUBSEQUENCE of "+ userInputTwo);
+		}
+		
+		
 	}// end of Strings
 	
 	/**
 	 * Get input from the user
+	 * Check if the string input from the user is a subsequence of the other string.
 	 * 
 	 */
 	public void input() {
 		
-		
+		String userInput;
+		char op;
 		Scanner scan = new Scanner (System.in);
-		String userInput = scan.nextLine();
 		
 		System.out.println("Would you like enter sequences manually or from a file? (M/F):" );
-		scan.nextLine();
-		/*
-		if( userInput.equals("F")) {
+		op = scan.nextLine().charAt(0);
+	
+		 if(op == 'M') {
 			
-			readFile();
-			closeFile();
-		}// end of if 1
-		*/
-		if(userInput.equals("M")) {
-			
-			Strings();
+			 manual();
 
-		}// end of else if
+		}// end of
+		 else if (op == 'F') {
+			 
+			 System.out.println("Enter the name of the file to process:");
+			 
+			 userInput = scan.nextLine();
+			 if(userInput == "sequences.txt") {
+				 readFile();
+			 }
+			 
+			 System.out.println("<END RUN>");
+			 System.exit(0);
+		 }
 		
-		if(userInput.equals("Y")) {
+		System.out.println();
+		System.out.println("Would you like to enter more sequences? (Y/N):" );
+		
+		op = scan.nextLine().charAt(0);
+		
+		if(op == 'Y') {
 			
-			Strings();
+			manual();
+			
+			//System.out.println();
+			//System.out.println("<END RUN>");
+		
 		}// end of if 2
-		else if (userInput.equals("N")) {
-			scan.close();
-		}
+		if (op == 'N') {
+			
+			System.out.println("<END RUN>");
+			System.exit(0);
+		}// end of  if 
+			
 		System.out.println("Would you like to enter more sequences? (Y/N):" );
-		scan.nextLine();
 		
+		op = scan.nextLine().charAt(0);
 		
-		System.out.println("Would you like to enter more sequences? (Y/N):" );
-		scan.nextLine();	
+	if(op == 'Y') {
+			
+			manual();
+			
+			//System.out.println();
+			//System.out.println("<END RUN>");
 		
+		}// end of if 2
+		if (op == 'N') {
+			
+			System.out.println("<END RUN>");
+			System.exit(0);
+		}// end of  if 
+		scan.close();
 	}// end of input
 	
-	public void openFile() {
-		
-		try {
-			
-			x = new Scanner (new File("file.txt"));
-		}// end of try
-		catch( Exception e) {
-			
-			System.out.println("No File Found");
-			
-		}// end of catch
-		
-		
-	}// end of openFile
 	
+	public void Stack() {
+	
+
+	}// end of Stack
+	
+	/**
+	 * A method reading a file using Scanner.
+	 */
 	public void readFile() {
 		
-		while(x.hasNext()) {
-			
-			String a = x.next();
-			String b = x.next();
-			String c = x.next();
-			String d = x.next();
-			
-			System.out.printf("%s %s %s %s", a, b, c , d);
-			
-		}// end of while
-	}// end of readFile
-	
-	public void closeFile() {
 		
-		x.close();
-	}
-	
+		try {
+			File seq = new File("C:\\Users\\maith\\Desktop\\BU-299\\CS-215-1\\Palindrome_MA\\file.txt");
+			
+			Scanner scan = new Scanner(seq);
+			
+			while(scan.hasNextLine());
+				System.out.println(scan);
+				
+				scan.close();
+		}// end of try
+		catch(FileNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		}// end of catch
+	}// end of readFile
 }// end of class
